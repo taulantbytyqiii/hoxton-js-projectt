cab04f18e11d1942b20f65244d292d8a63d9f0fc
taulant bytyqi <taulantbytyqi223@gmail.com>
2022-07-17 13:43:06 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// import "./style.css";
+
+//Create variables for the different parts of the page
+
+let header = document.querySelector("header");
+let filterAside = document.querySelector("aside");
+let prodSec = document.querySelector("section");
+let footer = document.querySelector("footer");
+
+//Create the function to create the header
+
+function createHeader() {}
