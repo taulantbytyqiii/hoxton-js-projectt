66e807867593fb60547bfa9abf51c038039fa6b6
taulant bytyqi <taulantbytyqi223@gmail.com>
2022-07-18 00:47:21 +0200

    Did quite somethings

--- main.js
+++ main.js
@@ -5,21 +5,28 @@
 let state = {
   products: [],
   userLoged: false,
+  categories: [],
+  searchInput: "",
 };
 
 //Variables for the different parts of the page
 
 let header = document.querySelector(".header");
-let filterAside = document.querySelector("aside");
-let prodSec = document.querySelector("section");
+let filterAside = document.querySelector(".filters-section");
+let prodSec = document.querySelector(".products-section");
 let footer = document.querySelector("footer");
+let modal = document.querySelector(".modal");
 
 //Main render function
 
 function render() {
   if (!header.childNodes.length) createHeader();
+  for (let product of state.products) {
+    prodSec.appendChild(createProduct(product));
+  }
 }
-render();
+// Get the products from database and call render();
+getProducts();
 
 //The function to create the header
 
@@ -79,4 +86,61 @@ function createRightHeaderSide() {
     headerRightSideContainer.append(shoppingCart, profileAvatar);
   }
   return headerRightSideContainer;
+}
+
+//Function to get the products from DB
+
+function getProducts() {
+  fetch("http://localhost:3006/products")
+    .then((resp) => resp.json())
+    .then((products) => {
+      state.products = products;
+      render();
+    });
+}
+
+//Function to create single product
+
+function createProduct(product) {
+  let prodDiv = document.createElement("div");
+  prodDiv.className = "product";
+  let prodImgWrapper = document.createElement("div");
+  prodImgWrapper.className = "product-image-wrapper";
+  let prodImg = document.createElement("img");
+  prodImg.src = product.image;
+  prodImg.className = "product-image";
+  let prodName = document.createElement("h4");
+  prodName.className = "product-name";
+  prodName.textContent = product.name;
+  let stockDiv = document.createElement("div");
+  stockDiv.className = "stock";
+  let p1 = document.createElement("p");
+  p1.className = "on-stock";
+  p1.textContent = "On stock:";
+  let p2 = document.createElement("p");
+  p2.className =
+    product.stock > 3 ? "on-stock-number-green" : "on-stock-number-red";
+  p2.textContent = product.stock;
+  let prodPrice = document.createElement("p");
+  prodPrice.className = "product-price";
+  prodPrice.textContent = `$${product.price}`;
+  let prodSold = document.createElement("p");
+  prodSold.className = "sold";
+  prodSold.textContent = `${product.sold} sold`;
+
+  stockDiv.append(p1, p2);
+  prodImgWrapper.appendChild(prodImg);
+  prodDiv.append(prodImgWrapper, prodName, stockDiv, prodPrice, prodSold);
+
+  return prodDiv;
+}
+
+//Filter function that determines whether a product should be displayed
+
+function filterProducts(product) {
+  if (state.searchInput === "" && state.categories === null) return;
+  if (state.searchInput !== "" && state.categories !== null) {
+    if (product.name.includes(state.searchInput)) {
+    }
+  }
 }
